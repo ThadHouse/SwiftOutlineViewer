59c8f71335113ab502ba493ab1df9e9d37ebae42
Thad House <thadhouse1@gmail.com>
2022-03-26 18:23:55 -0700

    Initial Commit

---
+++ OutlineViewer/OutlineViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  OutlineViewerApp.swift
+//  OutlineViewer
+//
+//  Created by Thad House on 3/26/22.
+//
+
+import SwiftUI
+
+@main
+struct OutlineViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
